2d8bef0a4fead2a0d17380e61bc953e2f16011d1
Raihan Sikder <raihan.act@gmail.com>
2013-05-28 15:15:32 +0600

    added two new brands  EPSON, Brother

--- inc.functions.appspecific.php
+++ inc.functions.appspecific.php
@@ -1,7 +1,7 @@
 <?php
 include_once('inc.functions.appspecific.temp.php');
 
-$brands_array=array("Colgate", "Hills", "Adhoc","DEPP Admin", "Others","SEAT","HTC","HP","Grant Thorton","Corsodyl","HSBC","Mecca Bingo");
+$brands_array=array("Colgate", "Hills", "Adhoc","DEPP Admin", "Others","SEAT","HTC","HP","Grant Thorton","Corsodyl","HSBC","Mecca Bingo", "EPSON", "Brother");
 $deliverable_type_array=array("General flash banner", "Rich media flash banner", "Static banner", "Edm", "Website", "Others");
 $activity_type_array=array("Localization", "Adaptation", "Localization,Adaptation", "Master Development", "Master Development,Localization,Adaptation", "Others");
 $user_roles_array=array("Sr. Project Manager", "Project Manager", "Team Leader", "Sr. Developer", "Developer","Sr. Flash Designer","Flash Designer","Web Developer","Flash Developer", "QA Engineer", "Depp administration", "Others");
