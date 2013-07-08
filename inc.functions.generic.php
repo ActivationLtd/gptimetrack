c5bc79035609ebf763c79570ac6e7cc1a6f17cea
Raihan Sikder <raihan.act@gmail.com>
2013-07-08 14:37:23 +0600

    added pfizer as client add sorting option in createSelectOptionsFrmArray

--- inc.functions.generic.php
+++ inc.functions.generic.php
@@ -178,6 +178,7 @@ function createSelectOptions($dbtableName,$dbtableIdField,$dbtableValueField,$cu
 }
 
 function createSelectOptionsFrmArray($listArray,$selectedId,$name,$params){
+	$listArray=sort($listArray);
 	echo "<select name='$name' $params>";
 	echo "<option value=''>select</option>";
 	foreach($listArray as $item){
