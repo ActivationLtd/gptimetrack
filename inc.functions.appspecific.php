81f0f24fe6d3fa50f0cf89c84f2196e2015ea8c6
Raihan Sikder <raihan.act@gmail.com>
2013-07-15 16:59:09 +0600

    #

--- inc.functions.appspecific.php
+++ inc.functions.appspecific.php
@@ -3,7 +3,7 @@ include_once('inc.functions.appspecific.temp.php');
 
 $brands_array=array("Colgate", "Hills", "Adhoc","DEPP Admin", "Others","SEAT","HTC","HP","Grant Thorton","Corsodyl","HSBC","Mecca Bingo", "EPSON", "Brother","Verizon","PM Meeting","Holiday","Internal Time","Leave","Training","Betfair","Pfizer");
 $deliverable_type_array=array("Standard flash banner", "Rich media flash banner", "Static banner", "Edm", "Website", "Others","PM Meeting","Holiday","Internal time","Leave","Training",);
-$activity_type_array=array("Localization", "Adaptation", "Localization,Adaptation", "Master Development", "Master Development,Localization,Adaptation", "Others");
+$activity_type_array=array("Localization", "Adaptation", "Localization,Adaptation", "Master Development", "Master Development,Localization,Adaptation", "Others","Depp Administration");
 $user_roles_array=array("Sr. Project Manager", "Project Manager", "Team Leader", "Sr. Developer", "Developer","Web Developer -  HTML/Web Developer","Sr. Flash Designer","Flash Designer","Web Developer","Flash Developer", "QA Engineer", "Depp administration", "Others");
 $project_billing_type_array=array("Retained", "Non-retained");
 $time_activity_type_array=array("Project Management", "Development", "QA", "PM Meeting", "Holiday", "Internal Time", "Leave", "Training","Depp Administration");
